3b36f6c191513d951bf094b3f57826ae16ad24dd
Murillo77 <murillofsantanna@gmail.com>
2025-05-27 14:26:56 -0300

    Tela inicial criada.

--- Gaia/App.js
+++ Gaia/App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}>Seja Bem-Vindo à</Text>
+      <Text style={styles.subtitulo}>Gaia</Text>
+      <TouchableOpacity style={styles.botao}>
+        <Text style={styles.textoBotao}>Cadastrar-se</Text>
+      </TouchableOpacity>
+      <Text style={styles.text}>Já possui cadastro? Entre</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,40 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    padding:30,
+    backgroundColor: '#56A829',
     justifyContent: 'center',
   },
+  titulo: {
+    fontSize:39,
+    color:'#1F1F1F',
+    fontWeight:'bold'
+
+  },
+  subtitulo: {
+    fontSize:50,
+    color:'#CBE3BF',
+    fontWeight:'bold'
+  },
+  botao: {
+    backgroundColor: '#1F1F1F',
+    paddingVertical: 14,
+    paddingHorizontal: 28,
+    borderRadius: 12,
+    elevation: 3,
+    top:100
+  },
+  textoBotao: {
+    color: '#CBE3BF',
+    fontSize: 16,
+    fontWeight: '600',
+    textAlign:'center'
+  },
+  text:{
+    color: '#CBE3BF',
+    fontWeight: '600',
+    fontSize: 15,
+    top:110,
+    textAlign:'center'
+  }
 });
